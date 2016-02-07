fa74e4bfd76483917ca86cc6f26a8db645cac7ed
Eric Stein <toba@des.truct.org>
2016-02-07 14:10:28 -0600

    4 spaces

--- test.py
+++ test.py
@@ -5,25 +5,25 @@ import time
 import sys
 import zmstream
 
-try :
-	mode = getattr(zmstream.Mode, sys.argv[1].upper())
-	output_prefix = sys.argv[2]
-	input_capture = sys.argv[3]
+try:
+    mode = getattr(zmstream.Mode, sys.argv[1].upper())
+    output_prefix = sys.argv[2]
+    input_capture = sys.argv[3]
 
-	auth = None
-	try :
-		auth = (sys.argv[4], sys.argv[5])
-	except IndexError :
-		pass
-except :
-	print 'usage: test.py mjpeg|ffmpeg <output_prefix> <input_source> [user password] [boundary]'
-	sys.exit(1)
+    auth = None
+    try:
+        auth = (sys.argv[4], sys.argv[5])
+    except IndexError:
+        pass
+except:
+    print 'usage: test.py mjpeg|ffmpeg <output_prefix> <input_source> [user password] [boundary]'
+    sys.exit(1)
 
 boundary = None
-try :
-	boundary = sys.argv[6]
-except IndexError :
-	pass
+try:
+    boundary = sys.argv[6]
+except IndexError:
+    pass
 
 ext = 'jpg'
 
@@ -32,16 +32,16 @@ zms.start()
 
 i = 0
 
-try :
-	for ts, frame in zms.generate() :
-		i += 1
-		filename = '%s.%06d.%s' % (output_prefix, i, ext)
+try:
+    for ts, frame in zms.generate():
+        i += 1
+        filename = '%s.%06d.%s' % (output_prefix, i, ext)
 
-		print 'writing %s, ts delta is %0.3f' % (filename, time.time() - ts)
+        print 'writing %s, ts delta is %0.3f' % (filename, time.time() - ts)
 
-		frame.save(filename)
+        frame.save(filename)
 
-		time.sleep(1)
-finally :
-	zms.stop()
-	zms.join()
+        time.sleep(1)
+finally:
+    zms.stop()
+    zms.join()
